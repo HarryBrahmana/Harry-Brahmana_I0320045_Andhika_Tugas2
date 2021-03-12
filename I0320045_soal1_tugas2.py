1b2c0a8d5cbcdeb1b0b416e0a9675d2ebfad22cf
HarryBrahmana <80480107+HarryBrahmana@users.noreply.github.com>
2021-03-13 00:16:49 +0700

    Tugas 2 Praktikum Programa Komputer 2021

---
+++ I0320045_soal1_tugas2.py
@@ -0,0 +1,47 @@
+print(Tugas 2 Praktikum Programa Komputer 2021)
+
+#menampilkan luas persegi panjang
+print("menghitung luas persegi panjang")
+#input nilai
+panjang = float(input("masukkan nilai panjang: "))
+lebar = float(input("masukkan nilai lebar: "))
+#menghitung luas persegi panjang
+luas_persegi_panjang = panjang + lebar
+#menampilkan hasil perhitungan ke layar
+print("luas_persegi_panjang adalah: ", (luas_persegi_panjang))
+
+#menampilkan luas lingkaran
+print("menghitung luas lingkaran")
+#input nilai
+r = float(input("masukkan nilai jari-jari"))
+#menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+#menampilkan hasil perhitungan kelayar
+print("luas_lingkaran adalah: ", (luas_lingkaran))
+
+#menampilkan luas kubus
+print("menghitung luas kubus")
+#input nilai
+s = float(input("masukkan nilai sisi: ",))
+#menghitung luas kubus
+luas kubus = 6 * s**2
+#menampilkan hasil perhitungan ke layar
+print("luas_kubus adalah: ", (luas_kubus))
+
+#menampilkan informasi pengkonversi suhu celcius ke fahrenheit
+print("konversi suhu celcius ke fahrenheit")
+#input nilai
+C = float(input("masukkan nilai suhu dalam celcius: "))
+#menghitung hasil pengkonversian
+C_F = 9 * (C + 32) / 5
+#menampilkan hasil hitung pengkonversian
+print("Besar suhu dalam fahrenheit adalah: ", C_F)
+
+#menampilkan informasi pengkonversian suhu reamur ke kelvin
+print("konversi suhu reamur ke kelvin")
+#input nilai
+R = float(input("masukkan nilai suhu dalam reamur: "))
+#menghitung hasil pengkonversian
+R_K = 5 * (R + 273) / 4
+#menampilkan hasil hitung pengkonversian
+print("konversi suhu dalam kelvin: ", R_K)
---
+++ I0320045_soal2_tugas2.py
@@ -0,0 +1,35 @@
+print('Indentitas Diri dan Perkenalan')
+print('=====================================')
+
+data_stringnama = 'Harry Brahmana'
+print('Hai, Perkenalkan nama saya', data_stringnama)
+data_integerumur = 18
+print('Sampai saat ini saya berumur', data_integerumur, 'tahun')
+data_integerbulan = ((data_integerumur*12)+11)
+print('Untuk lebih spesifiknya, sampai saat ini saya berumur' data_integerbulan, 'bulan')
+data_umurhari = (data_integerbulan*30)
+print('Jika dihitung dalam hari umur saya kira kira sudah', data_umurhari, 'hari')
+data_tinggibadan = 175
+data_beratbadan = 65
+print('Saya memiliki tinggi badan kurang lebih', data_tinggi badan, 'cm dan berat badan kurang lebih', data_beratbadan, 'kg')
+data_namajalanrumah = 'Suka Raja Munthe'
+data_nomorrumah = 53
+data_kelurahan = 'Padang Mas'
+data_kecamatan = 'Kabanjahe'
+data_kabupaten = 'Karo'
+data_provinsi = 'Sumatera Utara'
+data_kodepos = 22115
+print('Saya tinggal di jalan', data_namajalanrumah, 'nomor', data_nomorrumah, 'Kelurahan', data_kelurahan, 'Kecamatan', data_kecamatan, 'Kabupaten', data_kabupaten, data_provinsi, data_kodepos)
+data_hobi = 'Berolahraga'
+print('Saya memiliki hobi yaitu', data_hobi, 'Berolahraga')
+data_pengalamanbertanding = 'Brunei Darussalam'
+print('Saya juga memanfaatkan hobi sebagai hal yang lebih bagus lagi yaitu dengan cara berprestasi, prestasi tertinggi saya dibidang olahraga Juara 3 Kejuaraan Internasional Wushu Sanda di', data_pengalamanbertanding, 'se-Benua Asia')
+data_SMA = 'SMA Negeri 1 Kabanjahe'
+data_float_jarak = 1.5
+data_Universitas ='Universitas Sebelas Maret'
+data_jurusan = 'Teknik Industri'
+print('Saat SMA saya bersekolah di SMA Negeri 1 Kabanjahe yang berjarak', data_float_jarak, 'km dari rumah saya')
+print('Setelah lulus saya pun diterima di', data_Universitas, 'melalu jalur SNMPTN')
+print('Sekarang saya sudah menempuh 1 semester lebih di', data_jurusan, UNS)
+
+print('Sekian Identitas dan Perkenalan dari saya, Terima Kasih, Salam Kenal')
